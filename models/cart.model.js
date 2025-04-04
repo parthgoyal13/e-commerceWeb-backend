f449ee3a7a425fe31fe763a8aa24c75dde443e91
parthgoyal13 <nparthgoyal.skncoe.comp@gmail.com>
2025-04-04 17:14:25 +0530

    corrected cart schema

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  products: [
-    {
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Products" },
-      quantity: { type: Number, required: true, default: 1 },
-    },
-  ],
+  product: Object,
+  quantity: { type: Number, default: 1 },
 });
 
 const Cart = mongoose.model("Cart", cartSchema);
